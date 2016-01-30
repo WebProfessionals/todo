e8f3eb732534e6c4db723f55014f3b01cf4d2fc0
veith <veith.zaech@adcubum.com>
2016-01-30 10:22:51 +0100

    Snapshot

--- script.js
+++ script.js
@@ -1,24 +1,94 @@
-function cNachF() {
-    var eingabe = document.getElementById('eingabe');
-    var ausgabe = document.getElementById('resultat');
-
-// Wert aus input feld holen
-    var celsius = parseInt(eingabe.value);
-// FahrenhietWert rechnen
-    ausgabe.innerHTML = celsiusNachFahrenheit(celsius);
-}
+$(document).ready(function () {
+    'use strict';
+    projektListeLadenUndAufbauen();
+    //init
 
 
+    $('.projektauswahl').on('change', listeSetzen);
+
+    function listeSetzen() {
+        dieRiesengrosseListeDynamischLaden($(this).val());
+    }
+
+    function projektListeLadenUndAufbauen() {
+        $.ajax({
+            type: 'GET',
+            url: 'data/projektliste.json',
+            success: function (projektliste) {
+                dieRiesengrosseListeDynamischLaden(projektliste[0].id);
+                projektliste.forEach(function (element) {
+
+                    var $option = $('<option/>', {
+                        value: element.id,
+                        html: element.label
+                    });
+                    $('.projektauswahl').append($option);
+                })
+            },
+            error: function () {
+                alert("Der Server ist kapputt");
+            }
+        });
+    };
+
+    function dieRiesengrosseListeDynamischLaden(liste) {
+        $.ajax({
+            type: 'GET',
+            url: 'data/' + liste + '.json',
+            success: function (jsonBody) {
+                TasklisteAktualisieren('#taskliste', jsonBody);
+            },
+            error: function (error) {
+                alert('nicht gefunden');
+            }
+        });
+    }
 
 
-/**
- * Rechnet die Temp von C in F um
- * @param Celcius (number) Temp in °C
- * @returns {number}
- */
-function celsiusNachFahrenheit(Celcius) {
-    var Fahrenheit = Celcius * (9 / 5) + 32;
-    Celcius = Celcius * 44;
-    return Fahrenheit;
-}
+    /**
+     * Aktualisiert eine Liste
+     * @param selector (selector) Selektor für Zielelement and das die li angehängt werden
+     * @param daten (json) Daten
+     */
+    function TasklisteAktualisieren(selector, daten) {
+        var $taskliste = $(selector);
+        $taskliste.find('li').remove();
+        daten.tasks.forEach(function (element) {
 
+            var $task = $('<li/>', {
+                html: element.task,
+                class: istDerTaskErledigt(),
+                "data-taskid": element.id,
+                "data-erledigt": element.erledigt
+            });
+
+            function istDerTaskErledigt() {
+                if (element.erledigt) {
+                    return 'erledigt';
+                }
+                return 'offen';
+            }
+
+            $taskliste.append($task);
+        });
+    }
+
+    $('#taskliste').on('click', function (event) {
+        var $task = $(event.toElement);
+        var taskId = $task.data('taskid');
+
+        var neuerZustand = !$task.data('erledigt');
+
+        $task.data('erledigt', neuerZustand);
+        $task.toggleClass('erledigt');
+        taskAktualisieren(taskId, neuerZustand);
+    });
+
+
+    function taskAktualisieren(task, zielzustand) {
+        //TODO: Daten an den Server senden
+    //Todo:: sende die daten an den Server
+        console.dir(arguments);
+    }
+
+});
