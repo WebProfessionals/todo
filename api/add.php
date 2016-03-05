9df8440bf4aceed46bc967d9fb66ea4028d0dd4d
veith <veith.zaech@adcubum.com>
2016-03-05 10:42:53 +0100

    PHP Methoden fuer die Taskliste

---
+++ api/add.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: veith
+ * Date: 05.03.16
+ * Time: 10:32
+ */
+
+
+echo $_SESSION['n']++;
--- api/taskliste.php
+++ api/taskliste.php
@@ -1,5 +1,26 @@
 <?php
 session_start();
 
-echo $_SESSION['n']++;
+// entsprechende impl. laden
+switch ($_REQUEST['method']) {
+    case "add":
+        include("add.php");
+        break;
+    case "delete":
+        include("delete.php");
+        break;
+    case "read":
+        include("read.php");
+        break;
+    case "update":
+        include("update.php");
+        break;
+    case "createProjekt":
+        include("createProjekt.php");
+        break;
+    default:
+        header("HTTP/1.0 400 Unerlaubte Methode");
+
+        echo "Unerlaubte Methode";
+}
 
