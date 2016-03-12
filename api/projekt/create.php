09183ad77b5e7aca0ecf63230cec9ac06adf42ed
veith <veith.zaech@adcubum.com>
2016-03-12 11:25:52 +0100

    Serverseitig ein neues Projekt erstellen

--- api/projekt/create.php
+++ api/projekt/create.php
@@ -6,9 +6,26 @@
  * Time: 10:29
  */
 
+$projektname = strip_tags($_REQUEST['name']);
+
+// projektcounter laden
+$counterfile = "../geheim/projektcounter";
+$projektcounter = intval(file_get_contents($counterfile));
+$pljson = "../data/projektliste.json";
 
 // in projektliste.json das neue Projekt einfügen
+$projektliste = json_decode(file_get_contents($pljson), true);
+$projektliste[] = array("id" => $projektcounter, "label" => $projektname, "alleTasksErledigt" => false);
+file_put_contents($pljson, json_encode($projektliste));
+
 
 // eine leere taskliste erstellen
+umask(0);
+$taskliste = json_encode(array("projektname" =>$projektname,  "tasks"=> array(), "idcounter" => 1 ));
+file_put_contents("../data/{$projektcounter}.json",$taskliste);
+
+
+// neuen projektcounter speichern
+file_put_contents($counterfile, ++$projektcounter);
 
 // id oder ganze liste zurückgeben
